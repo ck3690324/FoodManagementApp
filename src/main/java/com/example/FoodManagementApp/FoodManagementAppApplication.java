af95cce2d46a1b202000a91a7b266054f177962c
ck.wong <ck3690324@gmail.com>
2026-06-12 16:00:34 +0900

    [Add] プロジェクト作成

---
+++ src/main/java/com/example/FoodManagementApp/FoodManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodManagementAppApplication.class, args);
+	}
+
+}
